9f5d643d5eb4466d774d84c1f480ebbb735cb021
Vasilii Razdorskii <vrazdorsky@yandex.ru>
2023-05-05 11:22:33 +0300

    Add arrow in Poster

--- src/components/Poster.js
+++ src/components/Poster.js
@@ -1,25 +1,29 @@
+import { VscArrowDown } from "react-icons/vsc";
+
 export default function Poster() {
   return (
     <section className="poster">
       <h1 className="poster__title">Все события города в один клик</h1>
       <ul className="poster__subtitle-list">
         <li>
-          <p className="poster__subtitle">Узнавай💡</p>
+          <p className="poster__subtitle">Узнавай 💡</p>
         </li>
         <li>
-          <p className="poster__subtitle">Лайкай❤️</p>
+          <p className="poster__subtitle">Лайкай ❤️</p>
         </li>
         <li>
-          <p className="poster__subtitle"> Посещай🌳</p>
+          <p className="poster__subtitle"> Посещай 🌳</p>
         </li>
         <li>
-          <p className="poster__subtitle"> Развивайся📈</p>
+          <p className="poster__subtitle"> Развивайся 📈</p>
         </li>
       </ul>
 
       <p className="poster__info-text">
-        официальный портал мероприятий города Москва.
+        Официальный портал мероприятий города Москва
       </p>
+
+      <VscArrowDown className="poster__arrow" />
     </section>
   );
 }
